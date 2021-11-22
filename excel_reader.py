5deccd30c7a0b3341ad97b538fa1248a92827bd4
imyashi0722 <imyash0722nps@gmail.com>
2021-11-22 20:59:40 +0530

    Still needs work on commented parts.

--- excel_reader.py
+++ excel_reader.py
@@ -1,5 +1,16 @@
-import os
-os.system("pip install xlrd")
-import xlrd
+# import os, os.system("pip install xlrd")
+# import xlrd
 
+class excel_reader_main():
+    def main(path):
+        return "responding"
 
+path = "D:\\Yashu\\Projects\\Python\\excel_reader\\Book.xlsx"
+
+#if path.endswith("xlsx"):
+#    import openpyxl as xl
+#    wb = xl.load_workbook("yourfile.xlsx")
+#    wb.save("file.xls")
+
+if type(path) == str:
+    print(excel_reader_main.main(path=path))
