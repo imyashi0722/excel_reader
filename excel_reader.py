09d228f8c18472621f6853d0168f74c9cdb73027
imyashi0722 <imyash0722nps@gmail.com>
2021-11-23 16:32:20 +0530

    malfuntioning still

--- excel_reader.py
+++ excel_reader.py
@@ -1,16 +1,20 @@
-# import os, os.system("pip install xlrd")
-# import xlrd
+import os
+import pyexcel
+from pyexcel.ext import xlsx
 
 class excel_reader_main():
     def main(path):
-        return "responding"
+        return excel_reader_main.format_check(path) 
 
-path = "D:\\Yashu\\Projects\\Python\\excel_reader\\Book.xlsx"
+    def format_check(path):
+        if path.endswith('xls') or path.endswith('xlsm') or path.endswith('xlsx'):
+            name = 'Test2.csv'
+            folder_path = file_name=os.path.dirname(path)
+            pyexcel.save_as(folder_path, dest_file_name=folder_path+name)
+            return 'saved as {0}'.format(name)
+        else: return 'Unsupported format'
 
-#if path.endswith("xlsx"):
-#    import openpyxl as xl
-#    wb = xl.load_workbook("yourfile.xlsx")
-#    wb.save("file.xls")
+path = r'D:/Yashu/Projects/Python/excel_reader/Book.xlsx'
 
 if type(path) == str:
     print(excel_reader_main.main(path=path))
